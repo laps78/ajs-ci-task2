3932de9b831fcc88462c9458aaf5d6ae3d300ba2
laps78 <9254384@mail.ru>
2022-09-07 17:20:10 +0300

    Test file added to index of git

---
+++ src/js/__tests__/sortHeroes.test.js
@@ -0,0 +1,16 @@
+import sortHeroes from '../app';
+
+test('sortHeroes should return sorted Array', () => {
+  const incommingArray = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const result = sortHeroes(incommingArray);
+
+  expect(result).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
